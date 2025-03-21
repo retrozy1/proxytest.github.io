d31c393664ac06450c841b078bf730b90b982150
retrozy1 <retrozybetrozy@outlook.com>
2025-03-21 11:35:19 -0400

    Update all.js

--- all.js
+++ all.js
@@ -1,6 +1,19 @@
-document.getElementById('sessionButton').addEventListener('click', function() {
-    const action = this.getAttribute('data-action');
-    if (action === 'save') {
-        alert('Save button clicked!');
+/* (async () => {
+    async function post(endpoint, params){
+        return fetch(`/api/v2/${endpoint}`, {
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(params),
+            method: "POST"
+        }).then(x => x.json());
     }
-});
+    
+    document.getElementById('sessionButton').addEventListener('click', function() {
+        const action = this.getAttribute('data-action');
+        if (action === 'enter') {
+            
+        }
+    });
+})();
+*/
+
+document.write("heelo!");
